429947ec378c836fde90b5543becde3f91f57bf6
Bryan Lee <bryan.lee.jh@gmail.com>
2024-11-27 00:56:31 +0800

    fix: fix deploy.yml

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/weather-app/",
+  define: {
+    "process.env": process.env,
+  },
   plugins: [react()],
   resolve: {
     alias: {
